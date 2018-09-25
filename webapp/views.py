8345567dbf9c8a3c3e5ca9fa93702428905626b0
Giacomo Carnovalini <bleckjeck90@gmail.com>
2018-09-25 12:59:11 +0100

    static folder setup w/ url_for

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import (
+  render_template,
+  url_for
+)
 from . import app
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', title="Home")
 
 @app.route("/about")
 def about():
-  return render_template('about.html')
+  return render_template('about.html', title="About")
