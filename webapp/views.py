bcf82ed8ae383d84b0dcd7f204f421b0b8372891
Giacomo Carnovalini <bleckjeck90@gmail.com>
2018-09-25 14:41:44 +0100

    added login form & flash messages

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,11 @@
 from flask import (
   render_template,
-  url_for
+  url_for,
+  flash,
+  redirect
 )
 from . import app
+from .forms import LoginForm
 
 @app.route("/")
 @app.route("/home")
@@ -12,3 +15,15 @@ def home():
 @app.route("/about")
 def about():
   return render_template('about.html', title="About")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    # Dummy data to simulate login
+    if form.email.data == 'test@test.com' and form.password.data == '1234':
+      flash(f'{form.email.data} logged in succesfully!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Invalid Email/Password', 'error')
+  return render_template('login.html', title="Login", form=form)
