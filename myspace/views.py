38e0820557fa1e4f0ea0ad50acb024cd274421b5
Giacomo Carnovalini <bleckjeck90@gmail.com>
2018-10-13 14:33:25 +0100

    added custom error pages

--- myspace/views.py
+++ myspace/views.py
@@ -66,6 +66,20 @@ def login():
       flash('Login Unsuccessful. Please check username and password', 'error')
   return render_template('login.html', title="Login", form=form)
 
+# ERROR HANDLING
+@app.errorhandler(404)
+def error_404(error):
+  return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def error_403(error):
+  return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def error_500(error):
+  return render_template('500.html'), 500
+
+
 #########
 # PRIVATE ROUTES
 #########
