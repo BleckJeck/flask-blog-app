90f4f933a40fd4ce9b9ae3973f63f09a7b08ff2f
Giacomo Carnovalini <bleckjeck90@gmail.com>
2018-10-20 23:02:36 +0100

    added landing page

--- myspace/views.py
+++ myspace/views.py
@@ -28,14 +28,17 @@ def is_logged_in(f):
 #########
 
 @app.route("/")
+def landing():
+  return render_template('landing-page.html')
+
 @app.route("/home")
 def home():
   return render_template('home.html', title="Home")
 
 
-@app.route("/about")
-def about():
-  return render_template('about.html', title="About")
+@app.route("/docs")
+def documentation():
+  return render_template('docs.html', title="Docs")
 
 
 @app.route("/blog")
