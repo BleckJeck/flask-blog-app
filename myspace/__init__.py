827e544b8284108296c10c4f4b37b37b153b40d2
Giacomo Carnovalini <bleckjeck90@gmail.com>
2018-12-01 16:46:13 +0000

    random cleanup

--- myspace/__init__.py
+++ myspace/__init__.py
@@ -40,16 +40,16 @@ app.register_blueprint(trackme, url_prefix='/trackme')
 
 @app.errorhandler(403)
 def error_403(error):
-  return render_template('errors/403.html'), 403
+  return render_template('errors/403.html', title="403"), 403
 
 @app.errorhandler(404)
 def error_404(error):
-  return render_template('errors/404.html'), 404
+  return render_template('errors/404.html', title="404"), 404
 
 @app.errorhandler(405)
 def error_405(error):
-  return render_template('errors/405.html'), 405
+  return render_template('errors/405.html', title="405"), 405
 
 @app.errorhandler(500)
 def error_500(error):
-  return render_template('errors/500.html'), 500
+  return render_template('errors/500.html', title="500"), 500
